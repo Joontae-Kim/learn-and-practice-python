1fac5682194f76b3c185ac24164480152fcedd79
JOON_THINKPAD\bexel <bexellenting@gmail.com>
2023-12-21 01:58:43 +0900

    2023년 11월 21일

---
+++ 2023-11-21.py
@@ -0,0 +1,40 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+1 + 1
+
+0.029411764705882353 * 34
+
+# +
+# 나눗셈 후 소수점 이하를 버리는 // 연산자
+# -
+
+5.5 // 2
+
+# 나눗셈 후 나머지를 구하는 % 연산자
+5 % 5
+# 3 % 2
+
+# 거듭제곱을 구하는 ** 연산자
+5 ** 2
+
+# 값을 정수로 만들기
+int(3.3) # int('1.4')
+
+int(False) # int(True)
+
+# 몫과 나머지를 함께 구하기
+quotient, remainder = divmod(5, 2)
+quotient, remainder
